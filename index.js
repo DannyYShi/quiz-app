7fbb7588532d93ff2b67822cdbb31b25006bf884
Danny Shi <dannyshi.sr@gmail.com>
2020-05-06 11:33:30 -0400

    added quiz question database

---
+++ index.js
@@ -0,0 +1,58 @@
+// question database
+const STORE = [
+    {
+        question: 'What does # stand for in music theory?',
+        answers: [
+          'hashtag',
+          'pound sign',
+          'sharp',
+          'flat'
+        ],
+        correctAnswer:
+          'sharp'
+      },
+      {
+        question: 'What is the opposite of crescendo?',
+        answers: [
+          'fortissimo',
+          'poco',
+          'loco',
+          'diminuendo'
+        ],
+        correctAnswer:
+          'diminuendo'
+      },
+      {
+        question: 'A dotted half note is equal to how many quarter notes?',
+        answers: [
+          '4',
+          '3',
+          '2',
+          '1'
+        ],
+        correctAnswer:
+          '3'
+      },
+      {
+        question: 'What is the musical term used to denote differences in volume?',
+        answers: [
+          'dynamics',
+          'harmony',
+          'resonance',
+          'loudness'
+        ],
+        correctAnswer:
+          'dynamics'
+      },
+      {
+        question: 'Which scale is the only scale that does not use sharps or flats?',
+        answers: [
+          'G major',
+          'F major',
+          'D major',
+          'C major'
+        ],
+        correctAnswer:
+          'C major'
+      },
+];
